eb3df68fd7a54992b6c59edd58a17e019311e73f
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2022-12-25 12:16:27 +0530

    Create sidebar2

---
+++ includes/sidebar2.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class='box box-solid' id="B2">
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title text-#2B2D2F'><b>Follow us on:</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+	  	</div>
+	</div>
+</div>
