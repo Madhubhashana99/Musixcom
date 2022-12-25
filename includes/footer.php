fa8baa5900fdae5330e514ef3152e731cb08e2bc
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2022-12-25 12:11:28 +0530

    Create footer

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+   
+    
+
+<footer class="main-footer" id="FC">
+    <!-- <div class="container-left">
+      <div class="pull-left ">
+      <p class="text-center">No 48<br>Main Street,<br>Matara.<br>0417925368/0765485695</p>
+      </div>  
+      <div class="pull-right ">
+      </div>       
+    </div> -->
+    <!-- <div class="container-left">
+      <div class="pull-center ">
+      <p class="text-center">No 5<br>Hiriketiya,<br>Beach Road,Dikwella.<br>0418532148/0714585695</p>
+      </div>   
+    </div>
+    <div class="container-left">
+      <div class="pull-right ">
+      <p class="text-left">No 441/5<br>Beach Road,<br>Weligama.<br>0414824734/0775485695</p>
+      </div>
+    </div> -->
+    <div class="container">
+      <div class="pull-center ">
+      <p class="text-center"><strong>Copyright © Musixlk.com 2022<br>All rights reserved</strong></p>
+      </div>   
+    </div>
+    
+</footer>
+    
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
