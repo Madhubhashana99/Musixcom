f2da2a953fbf50e152260ea70e75f0fcaa4973af
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2022-12-25 12:16:01 +0530

    Create sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,22 @@
+
+
+<div class="row">
+	<div class="box box-solid" id="B1">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title-center text-center text-white"><b>Subscribe to Our Newsletter</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	    	<p class="text-center">Get free updates about your favourite instrument and collect discounts and Vouchers.</p>
+	    	<form method="POST" action="">
+		    	<div class="input-group">
+	                <input type="text" class="form-control">
+	                <span class="input-group-btn">
+	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
+	                </span>
+	            </div>
+		    </form>
+	  	</div>
+	</div>
+</div>
+
+
